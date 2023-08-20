dcb91f1ad499cb5f1518725a3a5df13521ed39d0
Erick Dejo Vicente <61488294+mapachefurioso@users.noreply.github.com>
2023-08-19 23:46:35 -0500

    ReactStrap primer componente

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,13 @@
-import {  } from "react";
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
+import {HelloWorldComp,hola} from './HelloWorldApp';
+import BotonStrap from "./butonComponent";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+    <React.StrictMode>
+         <HelloWorldComp/>
+        <BotonStrap/>
+    </React.StrictMode>
+);
